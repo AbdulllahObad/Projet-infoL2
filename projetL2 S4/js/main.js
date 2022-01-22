9c523fd4b7ef0a1d22fd00acb85447d42a3bf87f
AliMohammed232000 <92219822+AliMohammed232000@users.noreply.github.com>
2022-01-22 20:20:22 +0100

    select changes
    j'essaie de faire select et option compatible avec tous fichiers json

--- projetL2 S4/js/main.js
+++ projetL2 S4/js/main.js
@@ -24,3 +24,42 @@ $(document).ready(function(){
     });
 
 });
+
+
+
+
+
+/*********************************************************************************/
+//Ali select tag
+
+
+
+/*function select(){ //for the select part
+    $.getjson(jeu1.json, function(data){
+        let n=1;
+
+        $.each(data, function(i, o){
+        for(let i of data["possibilites"]){ 
+            for(let j in data["possibilites"][i]){ 
+
+            $(#list1).append("<option value='n'>j</option> <br>");
+            n++;}}
+        }
+
+    });
+}
+
+function selection(liste){ //for the potion part
+
+    console.log(liste.options[liste.options.selectedIndex].value);
+
+    /*if(liste.options[liste.options.selectedIndex].value == "2"){
+        $("#list2").append("<option value='6'>Homme</option> <option value='7'> Femme</option>");
+    }
+
+
+
+    
+
+
+}*/
