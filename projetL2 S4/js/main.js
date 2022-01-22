3f8039ebe9a7919e8baa0e23e5578f53a1b623e5
acslwt <60791670+acslwt@users.noreply.github.com>
2022-01-22 18:29:00 +0100

    ss

--- projetL2 S4/js/main.js
+++ projetL2 S4/js/main.js
@@ -2,8 +2,24 @@ $(document).ready(function(){
 
     $.getJSON("js/jeu1.json",function(data){
 
-        console.log("Contenu : ");
-        console.log(data);
+        var nombres_lignes = data["ligne"];
+        var nombres_colonnes = data["colonne"];
+
+        for (var elements in data["possibilites"]) {
+
+            console.log(elements["0"]["prenom"]);
+
+            /*for(let i = 0;i<nombres_lignes;i++){
+
+                for(let j = 0;j<nombres_colonnes;j++){
+
+                    console.log(elements);
+
+                }
+
+            }*/
+
+        }
 
     });
 
