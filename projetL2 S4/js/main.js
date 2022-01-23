ed83dc7cd55a3458a5ae1f1db2c59b2003c38714
AbdulllahObad <95959487+AbdulllahObad@users.noreply.github.com>
2022-01-23 23:42:14 +0100

    start eliminer function

--- projetL2 S4/js/main.js
+++ projetL2 S4/js/main.js
@@ -1,22 +1,22 @@
-function afficheListePerso(){
+function afficheListePerso() {
 
-    $.getJSON("js/jeu1.json",function(data){
+    $.getJSON("js/jeu1.json", function (data) {
 
         var lignes = data["ligne"];
         var colonnes = data["colonne"];
 
-        var all_characters =  $('.toutesPersonnages');
+        var all_characters = $('.toutesPersonnages');
 
-        for(let i = 0;i<lignes;i++){
+        for (let i = 0; i < lignes; i++) {
 
             var div = $("<div></div>");
-            var ligne = (div).attr('class','ligne');
+            var ligne = (div).attr('class', 'ligne');
 
-            for(let j = i+(5*i);(j-i-(5*i))<colonnes;j++){
+            for (let j = i + (5 * i); (j - i - (5 * i)) < colonnes; j++) {
 
-                var path_image = 'images/'+data["possibilites"][j]["fichier"];
+                var path_image = 'images/' + data["possibilites"][j]["fichier"];
 
-                ligne.append($("<img>").attr('src',path_image));
+                ligne.append($("<img>").attr('src', path_image));
 
             }
 
@@ -28,8 +28,15 @@ function afficheListePerso(){
 
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     afficheListePerso();
 
 });
+
+
+
+function eliminer() {
+    var x = document.getElementById('question').value;
+    console.log(x);
+}
