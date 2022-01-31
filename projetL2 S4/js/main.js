95d6e73aa1779806fc232edca840503eb004a962
acslwt <60791670+acslwt@users.noreply.github.com>
2022-01-31 17:14:51 +0100

    rien

--- projetL2 S4/js/main.js
+++ projetL2 S4/js/main.js
@@ -35,6 +35,7 @@ function compareCaracteristique(personnage_choisi, caracteristique, reponse) {
 $(document).ready(function () {
 
     $.getJSON("js/jeu1.json", function (data) {
+        
         question();
 
         var lignes = data["ligne"];
